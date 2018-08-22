b69d250e5861470771b39eb1859c50d407ba3ed1
Kathryn Schoephoester <kmschoep@usgs.gov>
2018-08-22 15:15:14 -0500

    abandon idea of running the bash script from mlr-legacy-liquibase, run script in this project instead.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM mdillon/postgis:9.6-alpine
+FROM mdillon/postgis:10-alpine
 
 LABEL David Steinich <drsteini@usgs.gov>
 
@@ -30,7 +30,12 @@ ENV LIQUIBASE_HOME /opt/liquibase
 ENV LOCALONLY "-c listen_addresses='127.0.0.1, ::1'"
 ENV LIQUIBASE_VERSION 3.5.3
 ENV POSTGRES_JDBC_VERSION postgresql-42.2.4.jar
-ENV MLR_LIQUIBASE_VERSION 1.1
+ENV MLR_LIQUIBASE_VERSION master
+ENV MLR_LEGACY_DATABASE_NAME mlr_legacy
+ENV MLR_LEGACY mlr_legacy
+ENV MLR_LEGACY_USER mlr_legacy_user
+ENV MLR_LEGACY_SCHEMA_NAME mlr_legacy_data
+
 
 RUN mkdir -p $LIQUIBASE_HOME
 RUN curl -Lk https://github.com/liquibase/liquibase/releases/download/liquibase-parent-$LIQUIBASE_VERSION/liquibase-$LIQUIBASE_VERSION-bin.tar.gz > liquibase.tar.gz && \
@@ -39,9 +44,9 @@ RUN curl -Lk https://github.com/liquibase/liquibase/releases/download/liquibase-
 
 RUN curl -Lk https://jdbc.postgresql.org/download/$POSTGRES_JDBC_VERSION > $LIQUIBASE_HOME/lib/postgresql.jar
 
-RUN curl -Lk https://github.com/USGS-CIDA/mlr-legacy-liquibase/archive/v$MLR_LIQUIBASE_VERSION.tar.gz > mlr-legacy-liquibase.tar.gz && \
-	tar -xzf mlr-legacy-liquibase.tar.gz -C $LIQUIBASE_HOME/ && \
-	rm mlr-legacy-liquibase.tar.gz 
+RUN curl -Lk https://github.com/USGS-CIDA/mlr-legacy-liquibase/archive/$MLR_LIQUIBASE_VERSION.zip > mlr-legacy-liquibase.zip && \
+	unzip mlr-legacy-liquibase.zip -d $LIQUIBASE_HOME/ && \
+	rm mlr-legacy-liquibase.zip 
 RUN mv $LIQUIBASE_HOME/mlr-legacy-liquibase-$MLR_LIQUIBASE_VERSION $LIQUIBASE_HOME/mlr-legacy-liquibase
 
 
@@ -49,7 +54,7 @@ RUN mv $LIQUIBASE_HOME/mlr-legacy-liquibase-$MLR_LIQUIBASE_VERSION $LIQUIBASE_HO
 # Grab Files to Configure Database with Liquibase
 ############################################
 
-COPY ./testData $LIQUIBASE_HOME/mlr-legacy-liquibase/mlr-liquibase/mlrLegacy/testData
+COPY ./testData $LIQUIBASE_HOME/mlr-legacy-liquibase/mlr-liquibase/mlr_legacy/testData
 
 COPY ./dbInit/1_run_liquibase.sh /docker-entrypoint-initdb.d/
 
--- dbInit/1_run_liquibase.sh
+++ dbInit/1_run_liquibase.sh
@@ -7,33 +7,41 @@ pg_ctl -D "$PGDATA" -m fast -o "$LOCALONLY" -w restart
 ${LIQUIBASE_HOME}/liquibase \
 --defaultsFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/postgres.properties \
 --classpath=${LIQUIBASE_HOME}/lib/postgresql.jar \
---changeLogFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/postgres/changeLog.yml \
+--changeLogFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/postgres/postgres/changeLog.yml \
+--url=jdbc:postgresql://127.0.0.1:5432/postgres \
 --logLevel=debug \
 update \
--DMLR_LEGACY_PASSWORD=${MLR_LEGACY_PASSWORD} 
+	-DPOSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+	-DMLR_LEGACY_PASSWORD=${MLR_LEGACY_PASSWORD} \
+	-DMLR_LEGACY_USER_PASSWORD=${MLR_LEGACY_USER_PASSWORD} 
 
 # application database create scripts
 ${LIQUIBASE_HOME}/liquibase \
 --defaultsFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/databaseCreate.properties \
 --classpath=${LIQUIBASE_HOME}/lib/postgresql.jar \
---changeLogFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/database/changeLog.yml \
+--changeLogFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/postgres/mlr_legacy/changeLog.yml \
+--url=jdbc:postgresql://127.0.0.1:5432/mlr_legacy \
 --logLevel=debug \
 update \
--DMLR_LEGACY_PASSWORD=${MLR_LEGACY_PASSWORD} 
+	-DPOSTGRES_PASSWORD=${POSTGRES_PASSWORD} 
 
 # application scripts
 ${LIQUIBASE_HOME}/liquibase \
 --defaultsFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/liquibase.properties \
 --classpath=${LIQUIBASE_HOME}/lib/postgresql.jar \
---changeLogFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/changeLog.yml \
+--changeLogFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/mlr_legacy/changeLog.yml \
+--url=jdbc:postgresql://127.0.0.1:5432/mlr_legacy \
 --logLevel=debug \
 update \
--DMLR_LEGACY_DATA_PASSWORD=${MLR_LEGACY_DATA_PASSWORD} -DMLR_LEGACY_USER_PASSWORD=${MLR_LEGACY_USER_PASSWORD} 
+	-DMLR_LEGACY_PASSWORD=${MLR_LEGACY_PASSWORD} \
+	-DMLR_LEGACY_USER_PASSWORD=${MLR_LEGACY_USER_PASSWORD} 
 
 echo "data load scripts"
 ${LIQUIBASE_HOME}/liquibase \
 --defaultsFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/liquibase.properties \
 --classpath=${LIQUIBASE_HOME}/lib/postgresql.jar \
---changeLogFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/mlrLegacy/testData/changeLog.yml \
+--changeLogFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/mlr_legacy/testData/changeLogAK.yml \
+--url=jdbc:postgresql://127.0.0.1:5432/mlr_legacy \
 --logLevel=debug \
-update 
+update \
+	-DMLR_LEGACY_PASSWORD=${MLR_LEGACY_PASSWORD}
