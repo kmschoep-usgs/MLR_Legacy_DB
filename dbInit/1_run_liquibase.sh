352ed7717dbe379f9dea07b7550fdaf6f2d623b7
Ivan Suftin <isuftin@gmail.com>
2018-11-15 12:40:44 -0600

    Updates to MLR Liquibase Docker config
    - Remove environments from docker compose file and leave it up to the env file to inject them in to the container
    - Add "PGPORT" env var to the docker-compose env file
    - Add "PGPORT" variable to the liquibase main shell script, default to 5432
    - Add proper shebang and shellcheck fix to shell scripts
    
    PGPORT is an environment variable used by Postgres itself to declare which port to serve on (see https://www.postgresql.org/docs/current/app-postgres.html )
    
    Adding this variable to the Docker container on startup allows Postgres to start on whatever port we like. Adding the variable to the liquibase shell scripts with a default allows liquibase to connect with a non-default port
    
    To change the port that Postgres starts on, update docker-compose.env to set it to something other than 5432

--- dbInit/1_run_liquibase.sh
+++ dbInit/1_run_liquibase.sh
@@ -1,47 +1,50 @@
-#!/bin/ash 
+#!/bin/ash
+# shellcheck shell=dash
 
 # Restart postgres to make sure we can connect
 pg_ctl -D "$PGDATA" -m fast -o "$LOCALONLY" -w restart
+
+PGPORT=${PGPORT:-5432}
 
 # superuser scripts
 ${LIQUIBASE_HOME}/liquibase \
 --defaultsFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/postgres.properties \
 --classpath=${LIQUIBASE_HOME}/lib/postgresql.jar \
 --changeLogFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/postgres/postgres/changeLog.yml \
---url=jdbc:postgresql://127.0.0.1:5432/postgres \
+--url=jdbc:postgresql://127.0.0.1:$PGPORT/postgres \
 --logLevel=debug \
 update \
 	-DPOSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
 	-DMLR_LEGACY_PASSWORD=${MLR_LEGACY_PASSWORD} \
-	-DMLR_LEGACY_USER_PASSWORD=${MLR_LEGACY_USER_PASSWORD} 
+	-DMLR_LEGACY_USER_PASSWORD=${MLR_LEGACY_USER_PASSWORD}
 
 # application database create scripts
 ${LIQUIBASE_HOME}/liquibase \
 --defaultsFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/databaseCreate.properties \
 --classpath=${LIQUIBASE_HOME}/lib/postgresql.jar \
 --changeLogFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/postgres/mlr_legacy/changeLog.yml \
---url=jdbc:postgresql://127.0.0.1:5432/mlr_legacy \
+--url=jdbc:postgresql://127.0.0.1:$PGPORT/mlr_legacy \
 --logLevel=debug \
 update \
-	-DPOSTGRES_PASSWORD=${POSTGRES_PASSWORD} 
+	-DPOSTGRES_PASSWORD=${POSTGRES_PASSWORD}
 
 # application scripts
 ${LIQUIBASE_HOME}/liquibase \
 --defaultsFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/liquibase.properties \
 --classpath=${LIQUIBASE_HOME}/lib/postgresql.jar \
 --changeLogFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/mlr_legacy/changeLog.yml \
---url=jdbc:postgresql://127.0.0.1:5432/mlr_legacy \
+--url=jdbc:postgresql://127.0.0.1:$PGPORT/mlr_legacy \
 --logLevel=debug \
 update \
 	-DMLR_LEGACY_PASSWORD=${MLR_LEGACY_PASSWORD} \
-	-DMLR_LEGACY_USER_PASSWORD=${MLR_LEGACY_USER_PASSWORD} 
+	-DMLR_LEGACY_USER_PASSWORD=${MLR_LEGACY_USER_PASSWORD}
 
 echo "data load scripts"
 ${LIQUIBASE_HOME}/liquibase \
 --defaultsFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/liquibase.properties \
 --classpath=${LIQUIBASE_HOME}/lib/postgresql.jar \
 --changeLogFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/mlr_legacy/testData/changeLogAK.yml \
---url=jdbc:postgresql://127.0.0.1:5432/mlr_legacy \
+--url=jdbc:postgresql://127.0.0.1:$PGPORT/mlr_legacy \
 --logLevel=debug \
 update \
 	-DMLR_LEGACY_PASSWORD=${MLR_LEGACY_PASSWORD}
