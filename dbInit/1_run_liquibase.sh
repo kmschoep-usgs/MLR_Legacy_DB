1a6a2cba83dfce19bd20fa15f78276bd2e153902
“mhines-usgs” <mhines@usgs.gov>
2018-12-07 11:55:55 -0600

    MLR-377: remove unused configs after changing init script

--- dbInit/1_run_liquibase.sh
+++ dbInit/1_run_liquibase.sh
@@ -8,7 +8,6 @@ PGPORT=${PGPORT:-5432}
 
 # superuser scripts
 ${LIQUIBASE_HOME}/liquibase \
---defaultsFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/postgres.properties \
 --classpath=${LIQUIBASE_HOME}/lib/postgresql.jar \
 --changeLogFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/postgres/postgres/changeLog.yml \
 --url=jdbc:postgresql://127.0.0.1:$PGPORT/postgres \
@@ -22,7 +21,6 @@ update \
 
 # application database create scripts
 ${LIQUIBASE_HOME}/liquibase \
---defaultsFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/databaseCreate.properties \
 --classpath=${LIQUIBASE_HOME}/lib/postgresql.jar \
 --changeLogFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/postgres/mlr_legacy/changeLog.yml \
 --username=postgres \
@@ -34,7 +32,6 @@ update \
 
 # application scripts
 ${LIQUIBASE_HOME}/liquibase \
---defaultsFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/liquibase.properties \
 --classpath=${LIQUIBASE_HOME}/lib/postgresql.jar \
 --changeLogFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/mlr_legacy/changeLog.yml \
 --url=jdbc:postgresql://127.0.0.1:$PGPORT/mlr_legacy \
@@ -47,7 +44,6 @@ update \
 
 echo "data load scripts"
 ${LIQUIBASE_HOME}/liquibase \
---defaultsFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/liquibase.properties \
 --classpath=${LIQUIBASE_HOME}/lib/postgresql.jar \
 --changeLogFile=${LIQUIBASE_HOME}/mlr-legacy-liquibase/mlr-liquibase/mlr_legacy/testData/changeLogAK.yml \
 --url=jdbc:postgresql://127.0.0.1:$PGPORT/mlr_legacy \
